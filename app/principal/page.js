5b6dcb017e1973ab14cc9c64649b9a3a89e86494
Rayssa__ <silveirarayssa06.07@gmail.com>
2026-02-26 20:13:38 -0300

    Páginas da Rayssa

---
+++ app/principal/page.js
@@ -0,0 +1,103 @@
+import "./principal.css"
+
+export default function Principal() {
+    return (
+        <div class="row">
+            <div class="col-2" >
+                <div class="text-center">
+                    <img src="https://placehold.co/100"></img>
+                    <h1> Amigo da Vizinhança</h1>
+                </div>
+
+                <div class="list-group list-group-flush">
+                    <a href="#" class="list-group-item list-group-item-action"> Home </a>
+                    <a href="#" class="list-group-item list-group-item-action"> Criar solicitação </a>
+                    <a href="#" class="list-group-item list-group-item-action"> Solicitações </a>
+                </div>
+            
+
+                <div class="text-center">
+                    <div>
+                        <button> <img src="https://placehold.co/25"></img> Perfil </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-9">
+                <h2> 🏚 Home </h2>
+
+                <div class="tst">
+                    <div className="card">
+                        <img src="https://placehold.co/100" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://placehold.co/100" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://placehold.co/100" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://placehold.co/100" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                </div>
+                <div class="tst">
+                    <div className="card">
+                        <img src="https://placehold.co/100" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://placehold.co/100" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://placehold.co/100" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://placehold.co/100" className="card-img-top" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
