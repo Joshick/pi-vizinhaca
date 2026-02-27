7df0f38033360bded86a9136b30023bfba10ac2e
Barbara <baferreranunes@gmail.com>
2026-02-26 21:29:10 -0300

    Login de Cadastro

--- app/cadastrar/page.js
+++ app/cadastrar/page.js
@@ -1,7 +1,81 @@
-export default function Cadastro(){
-    return(
+export default function Cadastro() {
+    return (
         <div>
-            <h1>Pagina de Cadastro da Barbara</h1>
+
+            <h1 class="text-center mt-5"> Cadastro De Usuários </h1>
+
+            <hr />
+
+            <form class="formCadastro text-center">
+
+                <label>
+                    Digite o nome:
+                    <br />
+                    <input required type="text" class="inputNome" />
+                </label>
+
+                <br /><br />
+
+                <label>
+                    Digite o e-mail:
+                    <br />
+                    <input required type="email" class="inputEmail" />
+                </label>
+
+                <br /><br />
+
+
+                <label>
+                    Digite a data de nascimento:
+                    <br />
+                    <input required type="date" class="inputData" />
+                </label>
+
+                <br /><br />
+
+                <label>
+                    Digite a Senha:
+                    <br />
+                    <input required type="password" class="inputSenha" />
+                </label>
+
+                <br /><br />
+
+                <label>
+                    Digite o Bairro:
+                    <br />
+                    <input required type="text" class="inputBairro" />
+                </label>
+
+                <br /><br />
+
+                <label>
+                    É Responsável?
+                    <br />
+                    <select required class="inputResponsavel">
+                        <option>Sim</option>
+                        <option>Não</option>
+                    </select>
+                </label>
+
+                <br /><br />
+
+                <label>
+                    É Premium?
+                    <br />
+                    <select required class="inputPremium">
+                        <option>Sim</option>
+                        <option>Não</option>
+                    </select>
+                </label>
+
+                <br /><br />
+
+                <button class="btn btn-outline-success me-2" type="submit">Salvar</button>
+                <button class="btn btn-outline-danger ms-2" type="reset">Cancelar</button>
+
+            </form>
+
         </div>
     )
 }
