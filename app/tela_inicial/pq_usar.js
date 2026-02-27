ce6d8a42f00d56d90f30a9fb22010895065bc0e8
Leonardo Naka <naka15225@gmail.com>
2026-02-27 20:59:24 -0300

    Atualizando os cards da tela inicial

--- app/tela_inicial/pq_usar.js
+++ app/tela_inicial/pq_usar.js
@@ -8,18 +8,19 @@ export default function Pq_usar() {
                     <div class="col">
 
                         <div class="card" >
-                            <img src="https://cdn-icons-png.flaticon.com/512/40/40054.png" class="card-img-top" alt=".." />
+                            <img src="https://cdn-icons-png.flaticon.com/512/40/40054.png" class="card-img-top" alt="..." />
                             <div class="card-body">
-                                <h5 class="card-title">title</h5>
-                                <p class="card-text">...</p>
+                                <h5 class="card-title">Relatos por Bairro</h5>
+                                <p class="card-text">Veja e reporte problemas do seu bairro</p>
 
                             </div>
                         </div>
+
                         <div class="card" >
-                            <img src="https://cdn-icons-png.flaticon.com/512/40/40054.png" class="card-img-top" alt="..." />
+                            <img src="https://cdn-icons-png.flaticon.com/512/40/40054.png" class="card-img-top" alt=".." />
                             <div class="card-body">
-                                <h5 class="card-title">title</h5>
-                                <p class="card-text">...</p>
+                                <h5 class="card-title">Priorização Democrática</h5>
+                                <p class="card-text">Os relatos mais votados ganham prioridade</p>
 
                             </div>
                         </div>
@@ -31,16 +32,16 @@ export default function Pq_usar() {
                         <div class="card" >
                             <img src="https://cdn-icons-png.flaticon.com/512/40/40054.png" class="card-img-top" alt="..." />
                             <div class="card-body">
-                                <h5 class="card-title">title</h5>
-                                <p class="card-text">...</p>
+                                <h5 class="card-title">Sistema de votação</h5>
+                                <p class="card-text">Vote nos problemas mais urgentes da sua região</p>
 
                             </div>
                         </div>
                         <div class="card" >
                             <img src="https://cdn-icons-png.flaticon.com/512/40/40054.png" class="card-img-top" alt="..." />
                             <div class="card-body">
-                                <h5 class="card-title">title</h5>
-                                <p class="card-text">...</p>
+                                <h5 class="card-title">Acompanhamento</h5>
+                                <p class="card-text">Acompanhe o status de resolução dos problemas</p>
 
                             </div>
                         </div>
