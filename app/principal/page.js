710594d77c6cbd240cac2ca773f2d66dac685bd2
Rayssa__ <silveirarayssa06.07@gmail.com>
2026-03-24 16:42:41 -0300

    Update 24/03/2026 - Adição modal criar, alterações css
    Update 24/03/2026 - Adição modal criar, alterações css

--- app/principal/page.js
+++ app/principal/page.js
@@ -129,17 +129,17 @@ export default function Principal() {
 
                 {/* LISTAGEM DAS PÁGINAS */}
                 <div className="list-group list-group-flush">
-                    <a href="#" className="list-group-item list-group-item-action"> Home </a>
-                    <a className="list-group-item list-group-item-action" data-bs-toggle="modal" data-bs-target="#modalCriar"> Criar solicitação </a>
-                    <a href="./usuarios" className="list-group-item list-group-item-action"> Usuários </a>
-                    <a href="./Bairros" className="list-group-item list-group-item-action"> Bairros </a>
-
+                    <a href="#" className="list-group-item list-group-item-action"><i className="bi bi-house"></i> Home </a>
+                    <a className="list-group-item list-group-item-action" data-bs-toggle="modal" data-bs-target="#modalCriar"><i class="bi bi-plus-lg"></i> Criar solicitação </a>
+                    <a href="./usuarios" className="list-group-item list-group-item-action"><i class="bi bi-people-fill"></i> Usuários </a>
+                    <a href="./Bairros" className="list-group-item list-group-item-action"><i class="bi bi-pin-map"></i> Bairros </a>
+                    <a href="./aprovacao" className="list-group-item list-group-item-action"><i class="bi bi-check-all"></i> Aprovações </a>
                 </div>
 
                 {/* PERFIL INFERIOR */}
                 <div className="text-center">
                     <div>
-                        <button> <img src="https://placehold.co/25"></img> Perfil </button>
+                        <button data-bs-toggle="modal" data-bs-target="#modalPerfil"> <i class="bi bi-person-circle"></i> Perfil </button>
                         <Link href="/"><button>Sair</button></Link>
                     </div>
                 </div>
@@ -149,14 +149,13 @@ export default function Principal() {
             <div className="col-9">
                 {/* INTRODUÇÃO */}
                 <div className="mt-3">
-                    <h2>
-                        <i class="bi bi-house"> </i> Home </h2>
+                    <h2><i className="bi bi-house"></i> Home </h2>
                 </div>
 
                 {/* CONTEÚDO PRINCIPAL */}
                 <div className="row mt-5 align-itens-center">
                     {/* PESQUISAR */}
-                    <div className="col-md-6">
+                    <div className="col-md-8">
                         <div className="input-group">
                             <input className="form-control" placeholder="Pesquisar solicitações..." />
                             <button className="btn btn-outline-secondary"> <i class="bi bi-search"></i> </button>
@@ -173,20 +172,18 @@ export default function Principal() {
                             <option> Pendente </option>
                             <option> Fechado </option>
                         </select>
-                        <div>
-
-                            {
-                                minhasSolicitacoes == true ?
-                                    <div>
-                                        <button className='btn btn-outline-secondary' onClick={botaoMinhasSolicitacoes}> Minhas Solicitações </button>
-                                    </div>
-                                :
-                                    <button className='btn btn-outline-secondary' onClick={botaoTodasSolicitacoes}> Todas Solicitações </button>
-                            }
-                        </div>
-
                     </div>
-
+                    {/* FILTRAR */}
+                    <div className="col-md-2">
+                        {
+                            minhasSolicitacoes == true ?
+                                <div>
+                                    <button className="btn btn-outline-secondary" onClick={botaoMinhasSolicitacoes}> Minhas Solicitações </button>
+                                </div>
+                                :
+                                <button className="btn btn-outline-secondary" onClick={botaoTodasSolicitacoes}> Todas Solicitações </button>
+                        }
+                    </div>
                 </div>
 
                 {/* CARDS SOLICITAÇÕES */}
@@ -198,7 +195,7 @@ export default function Principal() {
                                 <div className="align-itens-center">
                                     {/* <button className="btn">@{solicitacao.id_usuario.nome}</button> */}
                                     <button className="btn" onClick={() => excluir(solicitacao.id)}> Excluir </button>
-                                    <button className="btn"> Editar </button>
+                                    <button className="btn" data-bs-toggle="modal" data-bs-target="#modalEditar"> Editar </button>
                                 </div>
                                 <div className="card-body d-flex flex-column">
                                     <h5 className="card-title">{solicitacao.titulo}</h5>
@@ -212,49 +209,131 @@ export default function Principal() {
                         </div>
                     ))}
                 </div>
+            </div>
 
-                {/* MODAL CRIAR SOLICITAÇÕES */}
-                <div className="modal fade" id="modalCriar" tabIndex="-1">
-                    <div className="modal-dialog modal-dialog-scrollable">
-                        <div className="modal-content">
+            {/* MODAL CRIAR SOLICITAÇÕES */}
+            <div className="modal fade" id="modalCriar" tabIndex="-1">
+                <div className="modal-dialog modal-dialog-scrollable">
+                    <div className="modal-content">
 
-                            {/* TITULO MODAL */}
-                            <div className="modal-header">
-                                <h5 className="modal-title"> Nova Solicitação </h5>
-                                <button className="btn-close" data-bs-dismiss="modal"></button>
+                        {/* TITULO MODAL */}
+                        <div className="modal-header">
+                            <h5 className="modal-title"> Nova Solicitação </h5>
+                            <button className="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+
+                        {/* CORPO MODAL */}
+                        <div className="modal-body">
+
+                            {/* TITULO */}
+                            <div className="mb-3">
+                                <label className="form-label"> Título </label>
+                                <input onChange={e => alteraTitulo(e.target.value)} className="form-control" placeholder="Ex: Buraco na rua" />
                             </div>
 
-                            {/* CORPO MODAL */}
-                            <div className="modal-body">
-
-                                {/* TITULO */}
-                                <div className="mb-3">
-                                    <label className="form-label"> Título </label>
-                                    <input onChange={e => alteraTitulo(e.target.value)} className="form-control" placeholder="Ex: Buraco na rua" />
-                                </div>
-
-                                {/* DESCRIÇÃO */}
-                                <div className="mb-3">
-                                    <label className="form-label"> Descrição </label>
-                                    <textarea onChange={e => alteraDescricao(e.target.value)} className="form-control" rows="4" placeholder="Descreva o problema..."></textarea>
-                                </div>
-
-                                {/* IMAGEM */}
-                                <div className="mb-3">
-                                    <label className="form-label">Imagem</label>
-                                    <input onChange={e => alteraImagem(e.target.value)} type="file" className="form-control" />
-                                </div>
+                            {/* DESCRIÇÃO */}
+                            <div className="mb-3">
+                                <label className="form-label"> Descrição </label>
+                                <textarea onChange={e => alteraDescricao(e.target.value)} className="form-control" rows="4" placeholder="Descreva o problema..."></textarea>
                             </div>
 
-                            {/* RODAPÉ MODAL */}
-                            <div className="modal-footer">
-                                {/* BOTÃO ENVIAR */}
-                                <button onClick={salvar} className="btn btn-primary" data-bs-dismiss="modal"> Enviar Solicitação </button>
-                                {/* BOTÃO CANCELAR */}
-                                <button className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            {/* IMAGEM */}
+                            <div className="mb-3">
+                                <label className="form-label">Imagem</label>
+                                <input onChange={e => alteraImagem(e.target.value)} type="file" className="form-control" />
+                            </div>
+                        </div>
+
+                        {/* RODAPÉ MODAL */}
+                        <div className="modal-footer">
+                            {/* BOTÃO ENVIAR */}
+                            <button onClick={salvar} className="btn btn-primary" data-bs-dismiss="modal"> Enviar Solicitação </button>
+                            {/* BOTÃO CANCELAR */}
+                            <button className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            {/* MODAL PERFIL USUÁRIO */}
+            <div class="modal fade" id="modalPerfil" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Perfil do Usuário</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body text-center">
+                            <img src="https://placehold.co/100"></img>
+                            <h5>Nome do Usuário</h5>
+                            <p class="text-muted">cargo@empresa.com</p>
+                            <p>Departamento de Vendas</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                            <button type="button" class="btn btn-primary">Editar Perfil</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* MODAL EDITAR */}
+            <div className="modal fade" id="modalEditar" tabIndex="-1">
+                <div className="modal-dialog modal-dialog-scrollable">
+                    <div className="modal-content">
+
+                        {/* HEADER */}
+                        <div className="modal-header">
+                            <h5 className="modal-title">Editar Solicitação</h5>
+                            <button className="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+
+                        {/* BODY */}
+                        <div className="modal-body">
+
+                            {/* TÍTULO */}
+                            <div className="mb-3">
+                                <label className="form-label">Título</label>
+                                <input
+                                    // value={tituloEditar}
+                                    // onChange={e => setTituloEditar(e.target.value)}
+                                    className="form-control"
+                                />
+                            </div>
+
+                            {/* DESCRIÇÃO */}
+                            <div className="mb-3">
+                                <label className="form-label">Descrição</label>
+                                <textarea
+                                    // value={descricaoEditar}
+                                    // onChange={e => setDescricaoEditar(e.target.value)}
+                                    className="form-control"
+                                    rows="4"
+                                ></textarea>
+                            </div>
+
+                            {/* IMAGEM */}
+                            <div className="mb-3">
+                                <label className="form-label">URL da Imagem</label>
+                                <input
+                                    // value={imagemEditar}
+                                    // onChange={e => setImagemEditar(e.target.value)}
+                                    className="form-control"
+                                />
                             </div>
 
                         </div>
+
+                        {/* FOOTER */}
+                        <div className="modal-footer">
+                            <button
+                                // onClick={atualizar} 
+                                className="btn btn-primary" data-bs-dismiss="modal"> Salvar Alterações </button>
+
+                            <button className="btn btn-secondary" data-bs-dismiss="modal"> Cancelar </button>
+                        </div>
+
                     </div>
                 </div>
             </div>
