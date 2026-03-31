2277b9713a2d5a9e2474c368c05708e8a49133a4
Leonardo Naka <naka15225@gmail.com>
2026-03-31 18:54:18 -0300

    alteração modal do perfil no menu lateral

--- app/principal/menu_lateral.js
+++ app/principal/menu_lateral.js
@@ -2,32 +2,53 @@ import "./principal.css"
 
 
 function Menu_lateral() {
-    return ( 
+    return (
 
-            <div className="col-2" >
-               {/* LOGO + NOME */}
-                <div className="text-center mt-3">
-                    <img src="https://placehold.co/100"></img>
-                    <h1> Amigo da Vizinhança</h1>
-                </div>
+        <div className="col-2" >
+            {/* LOGO + NOME */}
+            <div className="text-center mt-3">
+                <img src="https://placehold.co/100"></img>
+                <h1> Amigo da Vizinhança</h1>
+            </div>
             {/* LISTAGEM DAS PÁGINAS */}
-                <div className="list-group list-group-flush">
-                    <a href="./principal" className="list-group-item list-group-item-action"><i className="bi bi-house"></i> Home </a>
-                    <a className="list-group-item list-group-item-action" data-bs-toggle="modal" data-bs-target="#modalCriar"><i className="bi bi-plus-lg"></i> Criar solicitação </a>
-                    <a href="./usuarios" className="list-group-item list-group-item-action"><i className="bi bi-people-fill"></i> Usuários </a>
-                    <a href="./Bairros" className="list-group-item list-group-item-action"><i className="bi bi-pin-map"></i> Bairros </a>
-                    <a href="./aprovacao" className="list-group-item list-group-item-action"><i className="bi bi-check-all"></i> Aprovações </a>
+            <div className="list-group list-group-flush">
+                <a href="./principal" className="list-group-item list-group-item-action"><i className="bi bi-house"></i> Home </a>
+                <a className="list-group-item list-group-item-action" data-bs-toggle="modal" data-bs-target="#modalCriar"><i className="bi bi-plus-lg"></i> Criar solicitação </a>
+                <a href="./usuarios" className="list-group-item list-group-item-action"><i className="bi bi-people-fill"></i> Usuários </a>
+                <a href="./Bairros" className="list-group-item list-group-item-action"><i className="bi bi-pin-map"></i> Bairros </a>
+                <a href="./aprovacao" className="list-group-item list-group-item-action"><i className="bi bi-check-all"></i> Aprovações </a>
+            </div>
+            {/* PERFIL INFERIOR */}
+            <div className="text-center">
+                <div>
+                    <button data-bs-toggle="modal" data-bs-target="#modalPerfil"> <i className="bi bi-person-circle"></i> Perfil </button>
+                    <a href="/" ><button>Sair</button></a>
                 </div>
-             {/* PERFIL INFERIOR */}
-                <div className="text-center">
-                    <div>
-                        <button  data-bs-toggle="modal" data-bs-target="#modalPerfil"> <i className="bi bi-person-circle"></i> Perfil </button> 
-                        <a href="/" ><button>Sair</button></a>
+            </div>
+            {/* MODAL PERFIL USUÁRIO */}
+            <div className="modal fade" id="modalPerfil" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Perfil do Usuário</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body text-center">
+                            <img src="https://placehold.co/100"></img>
+                            <h5> Rayssa Silveira </h5>
+                            <p className="text-muted">rayssa.admin@senac.com</p>
+                            <p> Administrador do sistema </p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                            <button type="button" className="btn btn-primary">Editar Perfil</button>
+                        </div>
                     </div>
                 </div>
             </div>
-       
-     );
+        </div>
+
+    );
 }
 
 export default Menu_lateral;
--- app/principal/page.js
+++ app/principal/page.js
@@ -326,27 +326,7 @@ export default function Principal() {
                 </div>
             </div>
 
-            {/* MODAL PERFIL USUÁRIO */}
-            <div className="modal fade" id="modalPerfil" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Perfil do Usuário</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body text-center">
-                            <img src="https://placehold.co/100"></img>
-                            <h5> Rayssa Silveira </h5>
-                            <p className="text-muted">rayssa.admin@senac.com</p>
-                            <p> Administrador do sistema </p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                            <button type="button" className="btn btn-primary">Editar Perfil</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+           
         </div>
     )
 }
